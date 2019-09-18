cae5f58c325ef56eba79a66792187318fd3cdc6c
sebastien <sebastien@sebastien.lan>
2019-09-18 22:48:02 +0200

    Create settings class

---
+++ app/src/main/java/core/Settings.java
@@ -0,0 +1,31 @@
+package core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Settings {
+
+    private String _pathFile;
+
+    public Settings(String pathFile)
+    {
+        this._pathFile = pathFile;
+    }
+
+    public Map<String, String> getAllSettings()
+    {
+        Map<String, String> settings = new HashMap<>();
+
+        return settings;
+    }
+
+    public void setNote()
+    {
+
+    }
+
+    public void createSettingsNodes()
+    {
+
+    }
+}
