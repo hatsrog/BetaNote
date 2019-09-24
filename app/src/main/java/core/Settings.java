329aef83da21d9825c2c194530cead030617a35f
sebastien <sebastien@sebastien.lan>
2019-09-24 23:04:50 +0200

    24092019-1

--- app/src/main/java/core/Settings.java
+++ app/src/main/java/core/Settings.java
@@ -1,13 +1,15 @@
 package core;
 
-import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Settings {
 
     public String createSettingsNodes()
     {
         String newSettings = "<<\n"+
-                                "lastModification:"+new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").toString()+"\n"+
+                                "title:\n"+
+                                "creationDate:"+ Calendar.getInstance().getTime()+"\n"+
+                                "lastModification:"+Calendar.getInstance().getTime()+"\n"+
                              ">>\n";
         return  newSettings;
     }
