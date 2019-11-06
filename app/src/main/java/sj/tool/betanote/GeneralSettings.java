d10ab9b084537525e874374e436b03fe6ee6eb99
hatsrog <sebastien@jourdan.info>
2019-11-06 23:43:41 +0100

    06112019-1

--- app/src/main/java/sj/tool/betanote/GeneralSettings.java
+++ app/src/main/java/sj/tool/betanote/GeneralSettings.java
@@ -1,13 +1,22 @@
 package sj.tool.betanote;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.List;
+
+import core.FileFinder;
+import helper.GenericConstants;
 
 public class GeneralSettings extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     protected void onCreate(Bundle savedInstanceState)
@@ -26,9 +35,51 @@ public class GeneralSettings extends AppCompatActivity implements NavigationView
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+    public void deleteAllNotes(View view)
     {
-        return false;
+        FileFinder fileFinder = new FileFinder();
+        List<String> allNotes = fileFinder.findLatestNotes(getFilesDir().toString() + "/"+ GenericConstants.BETANOTES_DIRECTORY);
+        if(allNotes.size() > 0)
+        {
+            for(String note : allNotes)
+            {
+                File fileToRemove = new File(getFilesDir().toString() + "/"+ GenericConstants.BETANOTES_DIRECTORY +"/"+note);
+                fileToRemove.delete();
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "Aucune note à supprimer", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem)
+    {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_new_betanote)
+        {
+            Intent intent = new Intent(GeneralSettings.this, Bnote.class);
+            intent.putExtra("newFile", "true");
+            startActivity(intent);
+        }
+        else if (id == R.id.nav_settings)
+        {
+            Intent intent = new Intent(GeneralSettings.this, GeneralSettings.class);
+            startActivity(intent);
+        }
+        else if (id == R.id.nav_help)
+        {
+            //! Créer un nouvel Intent avec une aide dediée
+        }
+        else if (id == R.id.nav_about) {
+            Intent intent = new Intent(GeneralSettings.this, About.class);
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
