15b765636cea6aed0c2205f865bf348d0f852258
hatsrog <sebastien@jourdan.info>
2020-04-20 15:09:15 +0200

    20042020-1

---
+++ app/src/main/java/helper/TypoConstants.java
@@ -0,0 +1,8 @@
+package helper;
+
+public final class TypoConstants
+{
+    public static final String UNDERLINE = "underline";
+    public static final String ITALIC = "italic";
+    public static final String BOLD = "bold";
+}
